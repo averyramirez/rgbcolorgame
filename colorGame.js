76dc60a8b8da3f8e4493c820ce611ffba58e1ef2
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-04-11 18:05:13 -0700

    Update colorGame.js

--- colorGame.js
+++ colorGame.js
@@ -31,11 +31,8 @@ function setupModeButtons(){
 
 function setupSquares(){
 	for(var i = 0; i < squares.length; i++){
-	//add click listeners to squares
 	squares[i].addEventListener("click", function(){
-		//grab color of clicked square
 		var clickedColor = this.style.background;
-		//comare color to pickedColor
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct!";
 			resetButton.textContent = "Play again?";
@@ -51,13 +48,10 @@ function setupSquares(){
 
 function reset(){
 	colors = generateRandomColors(numSquares);
-	//pick new random color from arr
 	pickedColor = pickColor();
-	//change color display to match picked
 	colorDisplay.textContent = pickedColor;
 	this.textContent = "New Colors";
 	messageDisplay.textContent = "";
-	//change colors of squares
 	for(var i = 0; i < squares.length; i++){
 		if(colors[i]){
 		squares[i].style.display = "block";
@@ -74,9 +68,7 @@ resetButton.addEventListener("click", function(){
 })
 
 function changeColors(color){
-	//loop through all squares
 	for(var i = 0; i < squares.length; i++){
-	//change each color to match given color
 	squares[i].style.background = color;
 	}
 }
@@ -87,23 +79,16 @@ function pickColor(){
 }
 
 function generateRandomColors(num){
-	//make an array
 	var arr = []
-	//repeat num times
 	for(var i = 0; i < num; i++){
-		//get random color and push into array
 		arr.push(randomColor())	
 	}
-	//return that array
 	return arr;
 }
 
 function randomColor(){
-	//pick a "red" from 0- 255
 	var r =Math.floor(Math.random() * 256);
-	//pick a "green" from 0- 255
 	var g =Math.floor(Math.random() * 256);
-	//pick a "blue" from 0- 255
 	var b =Math.floor(Math.random() * 256);
 	return "rgb(" + r + ", " + g +", " + b + ")";
 }
